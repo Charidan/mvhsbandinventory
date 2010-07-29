d3f38ae8c16c222ab6afdb9c7fd7c0ebdb0aa5a8
speedbreeze <speedbreeze@61a55b36-2ee7-8d4d-74e8-20864f2fa4dc>
2010-07-29 22:48:37 +0000

    Cleaned up brace use.

--- Application/src/mvhsbandinventory/InstrumentStoreEvent.java
+++ Application/src/mvhsbandinventory/InstrumentStoreEvent.java
@@ -6,14 +6,16 @@ import java.awt.AWTEvent;
  *
  * @author jonathan
  */
-public class InstrumentStoreEvent extends AWTEvent {
+public class InstrumentStoreEvent extends AWTEvent
+{
     public static final int INSTRUMENT_FIRST = AWTEvent.RESERVED_ID_MAX + 1;
     public static final int INSTRUMENT_ADDED = INSTRUMENT_FIRST;
     public static final int INSTRUMENT_MODIFIED = INSTRUMENT_FIRST + 1;
     public static final int INSTRUMENT_DELETED = INSTRUMENT_FIRST + 2;
     public static final int INSTRUMENT_LAST = INSTRUMENT_FIRST + 3;
 
-    public InstrumentStoreEvent (Instrument source, int id) {
+    public InstrumentStoreEvent (Instrument source, int id)
+    {
         super(source, id);
     }
 
