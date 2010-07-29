920143274c7a780c8422ceb46a001f09a5282050
speedbreeze <speedbreeze@61a55b36-2ee7-8d4d-74e8-20864f2fa4dc>
2010-07-29 22:48:37 +0000

    Cleaned up brace use.

--- trunk/Application/src/mvhsbandinventory/InstrumentStoreListener.java
+++ trunk/Application/src/mvhsbandinventory/InstrumentStoreListener.java
@@ -6,7 +6,8 @@ import java.util.EventListener;
  *
  * @author jonathan
  */
-public interface InstrumentStoreListener extends EventListener {
+public interface InstrumentStoreListener extends EventListener
+{
     public abstract void instrumentAdded(InstrumentStoreEvent e);
     public abstract void instrumentModified(InstrumentStoreEvent e);
     public abstract void instrumentDeleted(InstrumentStoreEvent e);
