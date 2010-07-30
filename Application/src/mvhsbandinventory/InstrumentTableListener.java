3870754ec23ea0d07f840c33dc6f35f5ed14901c
speedbreeze <speedbreeze@61a55b36-2ee7-8d4d-74e8-20864f2fa4dc>
2010-07-30 22:12:43 +0000

    Switched from using a mouse click event to display the selected instrument to using a SelectionListener.  This makes the arrow keys and initial table item selection work properly.

---
+++ Application/src/mvhsbandinventory/InstrumentTableListener.java
@@ -0,0 +1,30 @@
+package mvhsbandinventory;
+
+import javax.swing.JTable;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+/**
+ *
+ * @author jonathan
+ */
+public class InstrumentTableListener implements ListSelectionListener {
+    JTable table;
+    Display display;
+
+    public InstrumentTableListener (JTable table, Display display)
+    {
+        this.table = table;
+        this.display = display;
+    }
+
+    public void valueChanged(ListSelectionEvent e)
+    {
+        if (e.getSource() == table.getSelectionModel()
+                && table.getRowSelectionAllowed())
+        {
+            display.displayInstrument();
+        }
+    }
+
+}
