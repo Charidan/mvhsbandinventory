7eaa32ad3f15e5cce9124148ba6772dfeebae12f
speedbreeze <speedbreeze@61a55b36-2ee7-8d4d-74e8-20864f2fa4dc>
2010-08-09 21:48:30 +0000

    Adding the new data store event abstraction layer. InstrumentFileStoreListener.java is the listener that will be used to allow the GUI to update when changes are made to the data store.

---
+++ Application/src/mvhsbandinventory/InstrumentFileStoreListener.java
@@ -0,0 +1,25 @@
+package mvhsbandinventory;
+
+/**
+ *
+ * @author jonathan
+ */
+public class InstrumentFileStoreListener extends InstrumentStoreListener
+{
+
+    public void instrumentAdded(InstrumentStoreEvent event)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void instrumentRemoved(InstrumentStoreEvent event)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void instrumentModified(InstrumentStoreEvent event)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
