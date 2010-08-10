20574d760a22ced234d4d6411c6bb7bcc0d29cb3
speedbreeze <speedbreeze@61a55b36-2ee7-8d4d-74e8-20864f2fa4dc>
2010-08-10 21:44:47 +0000

    General code cleanup and bug fixes.

--- trunk/Application/src/mvhsbandinventory/InstrumentFileStore.java
+++ trunk/Application/src/mvhsbandinventory/InstrumentFileStore.java
@@ -11,8 +11,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import net.contentobjects.jnotify.JNotify;
 import net.contentobjects.jnotify.JNotifyException;
 import net.contentobjects.jnotify.JNotifyListener;
