a356ca525aa47cd147ac3ec7112e03c0a6897d25
speedbreeze <speedbreeze@61a55b36-2ee7-8d4d-74e8-20864f2fa4dc>
2010-08-10 19:36:12 +0000

    Added code to implement JNotify event handlers, load the instrument information, and fire the appropriate event in the InstrumentFileStore class.  InstrumentList and Display will then attach events to InstrumentFileStore and update accordingly.

--- trunk/Application/src/mvhsbandinventory/InstrumentFileStoreListener.java
+++ trunk/Application/src/mvhsbandinventory/InstrumentFileStoreListener.java
@@ -1,5 +1,6 @@
 package mvhsbandinventory;
 
+import java.io.File;
 import net.contentobjects.jnotify.JNotifyListener;
 
 /**
@@ -11,30 +12,36 @@ public class InstrumentFileStoreListener implements JNotifyListener
 
     private InstrumentFileStore store;
 
-    InstrumentFileStoreListener (InstrumentFileStore store)
+    public InstrumentFileStoreListener (InstrumentFileStore store)
     {
         this.store = store;
     }
 
     public void fileCreated (int wd, String root, String name)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        sendEvent(InstrumentStoreEvent.ADDED, root, name);
     }
 
     public void fileDeleted (int wd, String root, String name)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        sendEvent(InstrumentStoreEvent.REMOVED, root, name);
     }
 
     public void fileModified (int wd, String root, String name)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        sendEvent(InstrumentStoreEvent.MODIFIED, root, name);
     }
 
     public void fileRenamed (int wd, String root, String oldName, String newName)
     {
         throw new UnsupportedOperationException("Not supported; users aren't " +
                 "allowed to change the name, brand, or serial of instruments.");
+    }
+
+    private void sendEvent (int type, String root, String name) {
+        File changed = new File(root + File.pathSeparator + name);
+        Instrument instrument = store.read(changed);
+        store.fireEvent(type, instrument);
     }
 
 }
